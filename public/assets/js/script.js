bd1b6170c43f89041a5328ca2aba1d55de0a8239
FlorentViel <florviev@gmail.com>
2018-12-12 17:28:21 +0100

    add form login , login user , add script api in path : "script.js" ,  edit security.yaml, add link path and function twig "if is_garanted ('Role_User')" in "base.html.twig"

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -21,3 +21,75 @@ function close_nav() {
 // }
 
 
+// =============================================
+// API geo.api.gouv.fr. Retrieve City by ZipCode
+// =============================================
+
+//Variables
+var zipcode; 
+var source;
+var lists = {};
+var elmt_list = $('#user_register_city');
+
+
+
+$(document).ready(function(){
+
+    // ====================
+    // Retrieve lists
+    // ====================
+
+    $("#user_register_zipcode").blur(function(){
+
+        zipcode = $('#user_register_zipcode').val();
+        source = 'https://geo.api.gouv.fr/communes?codePostal='+zipcode+'&fields=nom&format=json&geometry=centre';
+        setLists(source);   
+        
+        
+        
+    });
+        
+
+});
+
+/**
+ * SetLists
+ * 
+ * @param (string) url address of Lists 
+ * @return (object) lists
+ */
+function setLists(url) 
+{
+    // AJAX request
+    $.ajax({
+        method: "GET",
+        url: url,
+        dataType: "json",
+        success: show_list,
+        error: function(){
+            alert( "Oops an error has occurred." );
+        }
+    });
+}
+
+
+// La partie DOM LIST
+function show_list(response) {
+
+    elmt_list.empty();
+    lists = response;
+    
+    $.each(lists, function (indexInArray, list) { 
+        
+
+        var elmt_option = $('<option>');
+    
+        elmt_option.attr("value", list.nom );
+        elmt_option.text(list.nom);
+        elmt_list.append(elmt_option); 
+    
+    });
+    
+}
+
+
