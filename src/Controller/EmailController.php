7ef3eb3d521ae6f399e5f127041d298ddbb4d0f8
guillaume-goubel <guillaume.goubel.pro@gmail.com>
2018-12-16 13:16:50 +0100

    parametrage envoi mail OK sur adresse primebag62@gmail  pass : Webforce3

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -9,31 +9,97 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmailController extends AbstractController
 {
+    // /**
+    //  * @Route("/email", name="email")
+    //  */
+    // public function email($name, \Swift_Mailer $mailer)
+    // {
+    //     $email = new Email();
+    //     $form = $this->createForm(EmailType::class, $email);
+
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+ 
+    //         $message = (new \Swift_Message('Bonjour Email'))
+    //             ->setSubject('Email de Primebag')
+    //             ->setFrom('primebag@gmail.com')
+    //             ->setTo('furyfatal@laposte.net')
+    //             ->setBody("Vous lisez mon email");
+
+    //         $mailer->send($message);
+
+    //         $email = $form->getData();
+
+    //         return $this->render('email/index.html.twig', [
+    //         ]);
+    //     }
+
+    // }
+
+
+
+    //PrimeBag62@gmail.com
+    //Pass : Webforce3
+    //https://stackoverflow.com/questions/39363613/swiftmailer-attachment-error-unable-to-open-file-for-reading-on-symfony2-proje
+
+
     /**
      * @Route("/email", name="email")
      */
-    public function email($name, \Swift_Mailer $mailer)
+    public function index(\Swift_Mailer $mailer)
     {
-        $email = new Email();
-        $form = $this->createForm(EmailType::class, $email);
 
-        $form->handleRequest($request);
+    $message = (new \Swift_Message('Hello Email'))
+        ->setFrom('primebag62@gmail.com')
+        ->setTo('primebag62@gmail.com')
+        ->setBody('Here is the message itself')
+        // ->attach(\Swift_Attachment::fromPath('my-document.pdf'))
+        /*
 
-        if ($form->isSubmitted() && $form->isValid()) {
- 
-            $message = (new \Swift_Message('Bonjour Email'))
-                ->setSubject('Email de Primebag')
-                ->setFrom('primebag@gmail.com')
-                ->setTo('furyfatal@laposte.net')
-                ->setBody("Vous lisez mon email");
+         RENDU DE LA PAGE HTML
 
-            $mailer->send($message);
+        ->setBody(
+            $this->renderView(
+                // templates/emails/registration.html.twig
+                'emails/registration.html.twig',
+                array('name' => $name)
+            ),
+            'text/html'
+        
+        
+         * If you also want to include a plaintext version of the message
+        ->addPart(
+            $this->renderView(
+                'emails/registration.txt.twig',
+                array('name' => $name)
+            ),
+            'text/plain'
+        )
+        */
+    ;
 
-            $email = $form->getData();
+    $mailer->send($message);
 
-            return $this->render('email/index.html.twig', [
-            ]);
-        }
+    return $this->render('email/index.html.twig', [
 
-    }
+    ]);
+
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
 }
